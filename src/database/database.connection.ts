be94cf436e0b936caf13c9eb39ee5097842d7993
joao <joaosb004@gmail.com>
2023-07-01 08:47:38 -0300

    Basic config

---
+++ src/database/database.connection.ts
@@ -0,0 +1,31 @@
+import pg from "pg";
+import { config } from "dotenv";
+
+const { Pool } = pg;
+config();
+
+const configDatabase = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: process.env.MODE === "prod",
+};
+
+console.log(process.env.DB_HOST);
+const db = new Pool(configDatabase);
+
+db.connect()
+    .then(client => {
+        console.log("Connected to PostgreSQL database");
+        client.release();
+    })
+    .catch(err => {
+        console.error(err);
+    });
+
+export default db;
+
+// export async function getPostById(id) {
+//     const client = await pool.connect();
+//     const data = pool.query(`SELECT id, user_id FROM posts WHERE id=$1`, [id]);
+//     client.release();
+//     return data;
+//   }
